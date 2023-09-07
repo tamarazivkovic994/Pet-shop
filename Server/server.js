8aeaf0c1c7821ee5cf32fee1043a49e70a035c09
Tamara Zivkovic <tamara.zivkovic009@gmail.com>
2023-09-07 12:45:55 +0200

    server part

---
+++ Server/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+import userRouter from "./routes/user.js";
+import tourRouter from "./routes/tour.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/users", userRouter); // http://localhost:5000/users/signup
+
+
+const MONGODB_URL = "mongodb://0.0.0.0:27017";
+const port = 5000;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
