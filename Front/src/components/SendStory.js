a6492768513c4a21362ded64de829751f082d923
Tamara Zivkovic <tamara.zivkovic009@gmail.com>
2023-10-24 22:54:55 +0200

    yt video

--- Front/src/components/SendStory.js
+++ Front/src/components/SendStory.js
@@ -108,6 +108,19 @@ const SendStory = () => {
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
+        <MDBCol md="6">
+          <div className="video-responsive">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/jc5XQSVeQ2c" 
+              title="YouTube video"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </MDBCol>
       </MDBRow>
     </MDBContainer>
   );
