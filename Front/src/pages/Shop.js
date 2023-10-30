f9cbd52424a951d0d7b39b4d7403351061a5c933
Tamara Zivkovic <tamara.zivkovic009@gmail.com>
2023-10-30 21:50:46 +0100

    loader added

--- Front/src/pages/Shop.js
+++ Front/src/pages/Shop.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { MDBCardGroup, MDBCard, MDBBtn } from "mdb-react-ui-kit";
 import items from "../data/items.json";
@@ -8,6 +8,8 @@ const Shop = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredItems, setFilteredItems] = useState(items);
   const [itemsToShow, setItemsToShow] = useState(8);
+  const [loading, setLoading] = useState(true);
+
   const dispatch = useDispatch();
 
   const handleSearch = (query) => {
@@ -19,76 +21,97 @@ const Shop = () => {
   };
 
   const handleAddToCart = (item) => {
-   dispatch(addItem(item));
+    dispatch(addItem(item));
   };
 
   const loadMore = () => {
     setItemsToShow((prevItems) => prevItems + 8);
   };
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
 
   return (
     <div>
-      <div
-        className="input-group rounded"
-        style={{ width: "20%", margin: "1%", paddingTop: '10%' }}
-      >
-        <input
-          type="search"
-          className="form-control rounded"
-          placeholder="Search"
-          aria-label="Search"
-          aria-describedby="search-addon"
-          value={searchQuery}
-          onChange={(e) => handleSearch(e.target.value)}
-        />
-        <span
-          className="input-group-text border-0"
-          id="search-addon"
-          style={{ gap: "20%" }}
+      {loading ? (
+        <div
+          className="d-flex justify-content-center mt-5"
+          style={{ paddingTop: "10%" }}
         >
-          <i
-            className="fas fa-search"
-            onClick={() => handleSearch(searchQuery)}
-          ></i>
-          <i className="fas fa-times" onClick={() => handleSearch("")}></i>
-        </span>
-      </div>
-      <MDBCardGroup className="m-3 cardGroupShop">
-        <div className="row">
-          {filteredItems.slice(0, itemsToShow).map((item) => (
-            <div key={item.id} className="col-md-3 d-flex align-items-stretch">
-              <MDBCard className="d-flex justify-content-center align-items-center cardItem">
-                <img
-                  src={item.image}
-                  className="card-img-top m-1 p-2 cardImage"
-                  alt={item.name}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{item.name}</h5>
-                  <p className="card-text">{item.details}</p>
-                  <p className="card-text">Price: ${item.price}</p>
-                </div>
-                <div>
-                  <MDBBtn
-                    className="d-flex justify-content-center m-1 btnCard"
-                    color="btn btn-dark"
-                    onClick={() => handleAddToCart(item)}
-                  >
-                    Add to cart
-                  </MDBBtn>
-                </div>
-              </MDBCard>
-            </div>
-          ))}
-        </div>
-        {itemsToShow < filteredItems.length && (
-          <div className="d-flex justify-content-center mt-2">
-            <MDBBtn className="btn btn-dark" onClick={loadMore}>
-              Load more
-            </MDBBtn>
+          <div className="spinner-border" role="status">
+            <span className="sr-only">Loading...</span>
           </div>
-        )}
-      </MDBCardGroup>
+        </div>
+      ) : (
+        <>
+          <div
+            className="input-group rounded"
+            style={{ width: "20%", margin: "1%", paddingTop: "10%" }}
+          >
+            <input
+              type="search"
+              className="form-control rounded"
+              placeholder="Search"
+              aria-label="Search"
+              aria-describedby="search-addon"
+              value={searchQuery}
+              onChange={(e) => handleSearch(e.target.value)}
+            />
+            <span
+              className="input-group-text border-0"
+              id="search-addon"
+              style={{ gap: "20%" }}
+            >
+              <i
+                className="fas fa-search"
+                onClick={() => handleSearch(searchQuery)}
+              ></i>
+              <i className="fas fa-times" onClick={() => handleSearch("")}></i>
+            </span>
+          </div>
+          <MDBCardGroup className="m-3 cardGroupShop">
+            <div className="row">
+              {filteredItems.slice(0, itemsToShow).map((item) => (
+                <div
+                  key={item.id}
+                  className="col-md-3 d-flex align-items-stretch"
+                >
+                  <MDBCard className="d-flex justify-content-center align-items-center cardItem">
+                    <img
+                      src={item.image}
+                      className="card-img-top m-1 p-2 cardImage"
+                      alt={item.name}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{item.name}</h5>
+                      <p className="card-text">{item.details}</p>
+                      <p className="card-text">Price: ${item.price}</p>
+                    </div>
+                    <div>
+                      <MDBBtn
+                        className="d-flex justify-content-center m-1 btnCard"
+                        color="btn btn-dark"
+                        onClick={() => handleAddToCart(item)}
+                      >
+                        Add to cart
+                      </MDBBtn>
+                    </div>
+                  </MDBCard>
+                </div>
+              ))}
+            </div>
+            {itemsToShow < filteredItems.length && (
+              <div className="d-flex justify-content-center mt-2">
+                <MDBBtn className="btn btn-dark" onClick={loadMore}>
+                  Load more
+                </MDBBtn>
+              </div>
+            )}
+          </MDBCardGroup>
+        </>
+      )}
     </div>
   );
 };
